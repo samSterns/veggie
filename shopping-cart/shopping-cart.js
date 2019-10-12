f6107acef863dfbedcc1c5e4017b628b9609603b
samSterns <sam.g.sterns@gmail.com>
2019-10-12 16:20:17 -0700

    calculates total

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -1,12 +1,11 @@
 import veggieArray from '../data/veggie.js';
 import { findById } from '../common/utils.js';
-// import{ calcOrderTotal, toUSD } from '../common/utils.js';
+import { calcOrderTotal, toUSD } from '../common/utils.js';
 import renderLineItem from './render-line-item.js';
 
 const tbody = document.querySelector('tbody');
-const placeOrderButton = document.getElementById('placeOrderButton');
-// const orderTotalCell = document.getElementById('order-total-cell');
-// const placeOrderButton = document.getElementById('place-order-button');
+const orderTotalCell = document.getElementById('order-total-cell');
+const placeOrderButton = document.getElementById('place-order-button');
 
 export const getCart = () => {
     console.log('made it here');
@@ -26,12 +25,6 @@ else if(placeOrderButton) {
     });
 }
 
-// if(placeOrderButton) {
-//     placeOrderButton.addEventListener('click', () => {
-//         alert('your cart contents are: ' + );
-//     });
-// }
-
 if(cart && tbody) {
     for(let i = 0; i < cart.length; i++) {
         const lineItem = cart[i];
@@ -41,11 +34,8 @@ if(cart && tbody) {
         tbody.appendChild(dom);
     } 
 }
-// } else if(placeOrderButton) {
-//     placeOrderButton.disabled = true;
-// }
 
-
-// const orderTotal = calcOrderTotal(cart, veggieArray);
-// orderTotalCell.textContent = toUSD(orderTotal);
-
+if(orderTotalCell) {
+    const orderTotal = calcOrderTotal(cart, veggieArray);
+    orderTotalCell.textContent = toUSD(orderTotal);
+}
