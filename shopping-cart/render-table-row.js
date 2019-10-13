e8733f63cff8d80e98d4bb52d89c842f1295a425
samSterns <sam.g.sterns@gmail.com>
2019-10-12 17:35:19 -0700

    sales table rendering with cart data

--- shopping-cart/render-table-row.js
+++ shopping-cart/render-table-row.js
@@ -1,36 +1,35 @@
 const makeTd = (content) => {
     const tdElement = document.createElement('td');
-    tdElement.textContent = content
+    tdElement.textContent = content;
 }
 
 
-export default (fruit, oder) => {
+export default (veggie, order) => {
     const tableRow = document.createElement('tr');
 
     const columnOne = document.createElement('td');
-    columnOne.textContent = 'apple'
+    columnOne.textContent = 'apple';
 
     const columnTwo = document.createElement('td');
-    columnOne.textContent = '4'
+    columnTwo.textContent = '4';
 
     const columnThree = document.createElement('td');
-    columnOne.textContent = '1.00'
+    columnThree.textContent = '1.00';
 
     const columnFour = document.createElement('td');
-    columnOne.textContent = '4.00'
+    columnFour.textContent = '4.00';
 
-    tableRow.appendChild(makeTd(fruit.name));
+    tableRow.appendChild(makeTd(veggie.name));
     tableRow.appendChild(makeTd(order.quantity));
-    tableRow.appendChild(makeTd(fruit.price));
-    const totalPrice = fruit.quantity * fruit.price;
+    tableRow.appendChild(makeTd(veggie.price));
+    const totalPrice = veggie.quantity * veggie.price;
     const prettyPrice = totalPrice.toLocaleString('en-US', { 
         style: 'currency', 
         currency: 'USD' })
     // const prettyPrice = totalPrice.toLocaleString('en-US', { 
-            style: 'currency', 
-            currency: 'USD' })
-    return tableRow
+    //         style: 'currency', 
+    //         currency: 'USD' });
+    return renderTableRow
 
     outterHtml: 'some string'
-    };
 };
